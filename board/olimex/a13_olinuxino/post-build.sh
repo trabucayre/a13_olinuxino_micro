314683bfe6a60587b07d56f82fbc23b1107f8931
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2017-09-06 10:27:59 +0200

    post-build.sh: do not hardcode BOARD_DIR path

--- board/olimex/a13_olinuxino/post-build.sh
+++ board/olimex/a13_olinuxino/post-build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+BOARD_DIR="$(dirname $0)"
 MKIMAGE=$HOST_DIR/bin/mkimage
-BOARD_DIR=${BR2_EXTERNAL_A13OLINUXINOMICRO_PATH}/board/olimex/a13_olinuxino
+BOOT_CMD=$BOARD_DIR/boot.cmd
+BOOT_CMD_H=$BINARIES_DIR/boot.scr
 
-$MKIMAGE -A arm -O linux -T script -C none \
-	 -d ${BOARD_DIR}/boot.cmd \
-	 ${BINARIES_DIR}/boot.scr
+# U-Boot script
+$MKIMAGE -A arm -O linux -T script -d $BOOT_CMD $BOOT_CMD_H
